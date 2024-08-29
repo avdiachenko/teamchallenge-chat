5b52814e1a7acf1ba7769ca343e1899604fb791a
Oleg <olegpl1993@gmail.com>
2024-08-29 23:55:14 +0300

    feat: signin form

--- frontend/src/pages/SignIn/SignIn.tsx
+++ frontend/src/pages/SignIn/SignIn.tsx
@@ -1,11 +1,63 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Header } from "../../widgets/Header/Header";
 import styles from "./SignIn.module.css";
 
+interface IFormInput {
+  name: string;
+  password: string;
+}
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.container}>
       <Header />
-      <div className={styles.content}>SignIn page</div>
+      <div className={styles.content}>
+        {" "}
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+          <div className={styles.inputContainer}>
+            <input
+              type="text"
+              {...register("name", {
+                required: "Name is required",
+                minLength: {
+                  value: 3,
+                  message: "Name must be at least 3 characters long",
+                },
+              })}
+              placeholder="Username"
+              className={styles.input}
+            />
+            {errors.name && <p className={styles.error}>{errors.name.message}</p>}
+          </div>
+
+          <div className={styles.inputContainer}>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters long",
+                },
+              })}
+              placeholder="Password"
+              className={styles.input}
+            />
+            {errors.password && <p className={styles.error}>{errors.password.message}</p>}
+          </div>
+
+          <input type="submit" value="Sign In" className={styles.submit} />
+        </form>
+      </div>
     </div>
   );
 }
