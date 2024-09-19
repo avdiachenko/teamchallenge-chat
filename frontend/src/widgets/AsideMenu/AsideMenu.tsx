d44e6ad726275a0542b9a04f6c8b7cca276cd28d
KraYFS <grigorievdaniil13@gmail.com>
2024-09-19 18:35:51 +0300

    feat: added checking side menu icons

--- frontend/src/widgets/AsideMenu/AsideMenu.tsx
+++ frontend/src/widgets/AsideMenu/AsideMenu.tsx
@@ -2,39 +2,14 @@ import { Link } from "react-router-dom";
 import { useUserStore } from "../../entities/user/user.store.ts";
 import styles from "./AsideMenu.module.css";
 import * as images from "./icons/index.ts";
+import { AsideMenuNav } from "./AsideMenuNav/AsideMenuNav.tsx";
 
 export function AsideMenu() {
   const { name, logout } = useUserStore();
 
   return (
     <aside className={styles.aside_menu}>
-      <nav className={styles.nav_menu}>
-        <div className={styles.main_links}>
-          <Link className={styles.nav_links} to="/chat">
-            <img src={images.ChatDots} alt="" />
-          </Link>
-          <Link className={styles.nav_links} to="/">
-            <img src={images.News} alt="" />
-          </Link>
-          <Link className={styles.nav_links} to="/">
-            <img src={images.Contact} alt="" />
-          </Link>
-          <Link className={styles.nav_links} to="/">
-            <img src={images.Cash} alt="" />
-          </Link>
-          <Link className={styles.nav_links} to="/">
-            <img src={images.People} alt="" />
-          </Link>
-          <Link className={styles.nav_links} to="/">
-            <img src={images.Settings} alt="" />
-          </Link>
-          {name && (
-            <button className={styles.nav_links_logout}>
-              <img onClick={logout} src={images.GoOut} alt="" />
-            </button>
-          )}
-        </div>
-      </nav>
+      {name ? <AsideMenuNav login={true} /> : <AsideMenuNav login={false} />}
     </aside>
   );
 }
