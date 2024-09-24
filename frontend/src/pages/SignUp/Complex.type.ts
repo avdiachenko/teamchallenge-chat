f1360b8f3fd82c3bc98906f1c810fc0ec87bc982
Oleg <olegpl1993@gmail.com>
2024-09-24 23:34:12 +0300

    feat: signUp add section field

---
+++ frontend/src/pages/SignUp/Complex.type.ts
@@ -0,0 +1,16 @@
+export interface Complex {
+  _id: string;
+  name: string;
+  images: string;
+  parking: boolean;
+  security: boolean;
+  access_control: boolean;
+  concierge: boolean;
+  playground: boolean;
+  closed_area: boolean;
+  video_surveillance: boolean;
+  description: string;
+  floors: number;
+  entrances: number;
+  location: string;
+}
