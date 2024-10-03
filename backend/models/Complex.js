9c668cb67ee4f8f5e8cdd90e8acf128bb688873f
Kalmarr-dev <art.hydrogen1405@gmail.com>
2024-10-03 21:25:35 +0300

    fix: images #3

--- backend/models/Complex.js
+++ backend/models/Complex.js
@@ -8,7 +8,7 @@ const residentialComplexSchema = new Schema(
       required: [true, "Name is required"],
     },
     images: {
-      type: Array[String],
+      type: Array,
       required: [true, "Image(s) is required"],
     },
     parking: {
