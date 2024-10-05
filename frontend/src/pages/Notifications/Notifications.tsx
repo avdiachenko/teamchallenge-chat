1303a0a470ea1c84b16ff24ce15260b59fcad1ee
Oleg <olegpl1993@gmail.com>
2024-10-05 23:03:52 +0300

    feat: notification layout

--- frontend/src/pages/Notifications/Notifications.tsx
+++ frontend/src/pages/Notifications/Notifications.tsx
@@ -1,9 +1,105 @@
+/* eslint-disable no-console */
+import { BaseButton } from "../../shared/components/BaseButton/BaseButton";
+import { AsideMenu } from "../../widgets/AsideMenu/AsideMenu";
+import { Header } from "../../widgets/Header/Header";
+import { NotificationCard } from "./NotificationCard/NotificationCard";
 import styles from "./Notifications.module.css";
+import { Notification } from "./notifications.types";
+
+const notifications: Notification[] = [
+  {
+    text: "We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids!",
+    type: "Events",
+    residential_complex: "Квартира 3",
+    updatedAt: "2024-06-20T15:39:24.000Z",
+  },
+  {
+    text: "Due to scheduled power outages, there will be a temporary electricity shutdown in sections 5 to 15 from 4:00 PM to 9:00 PM",
+    type: "Emergency messages",
+    residential_complex: "Квартира 3",
+    updatedAt: "2024-07-20T15:39:24.000Z",
+  },
+  {
+    text: "We remind you that it's time to make the September payments for the services of the maintenance company and security. Please make the payment as soon as possible to avoid any outstanding balances.",
+    type: "Payments",
+    residential_complex: "Квартира 3",
+    updatedAt: "2024-08-20T15:39:24.000Z",
+  },
+  {
+    text: "Due to scheduled power outages, there will be a temporary electricity shutdown in sections 5 to 15 from 4:00 PM to 9:00 PM",
+    type: "Emergency messages",
+    residential_complex: "Квартира 3",
+    updatedAt: "2024-09-20T15:39:24.000Z",
+  },
+  {
+    text: "We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances.",
+    type: "Events",
+    residential_complex: "Квартира 3",
+    updatedAt: "2024-10-05T15:39:24.000Z",
+  },
+  {
+    text: "Due to scheduled power outages, there will be a temporary electricity shutdown in sections 5 to 15 from 4:00 PM to 9:00 PM",
+    type: "Emergency messages",
+    residential_complex: "Квартира 3",
+    updatedAt: "2024-07-06T15:39:24.000Z",
+  },
+  {
+    text: "We remind you that it's time to make the September payments for the services of the maintenance company and security. Please make the payment as soon as possible to avoid any outstanding balances.",
+    type: "Payments",
+    residential_complex: "Квартира 3",
+    updatedAt: "2024-08-20T15:39:24.000Z",
+  },
+  {
+    text: "We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances. We invite you and your children to a festive gathering to celebrate a birthday at 12:00 p.m at the main playground. A fun foam party awaits the kids! In the meantime, we encourage you to make the payment as soon as possible to avoid any outstanding balances.",
+    type: "Events",
+    residential_complex: "Квартира 3",
+    updatedAt: "2024-10-05T15:39:24.000Z",
+  },
+];
 
 export function Notifications() {
+  // const { data: notifications } = useApi<Notification[]>("/notifications");
+
+  const handleSendMessage = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const message = e.currentTarget.value;
+      console.log(message);
+      e.currentTarget.value = "";
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <span className={styles.title}>Notifications</span>
-    </div>
+    <>
+      <AsideMenu />
+      <Header title="Notifications" />
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.notifications}>
+            <div className={styles.wrapper}>
+              <div className={styles.menu}>
+                <BaseButton variant="quaternary">Notifications</BaseButton>
+                <BaseButton variant="quaternary">Voting</BaseButton>
+                <BaseButton variant="quaternary">News</BaseButton>
+              </div>
+
+              <div className={styles.notificationList}>
+                {notifications.map((notification, index) => (
+                  <NotificationCard key={index} notification={notification} />
+                ))}
+              </div>
+
+              <div className={styles.inputContainer}>
+                <input
+                  className={styles.input}
+                  type="text"
+                  placeholder={"Type your message here"}
+                  onKeyDown={handleSendMessage}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
