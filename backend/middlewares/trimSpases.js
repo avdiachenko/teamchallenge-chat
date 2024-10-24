f19a4968afa3a12c747d3cbb6a7ece8271df724b
VictorFil67 <victorfilinovych@gmail.com>
2024-10-24 22:27:25 +0300

    updated trimSpases

--- backend/middlewares/trimSpases.js
+++ backend/middlewares/trimSpases.js
@@ -1,30 +1,30 @@
 function trimSpases(req, res, next) {
-  const { name, email, password } = req.body;
-  if (name) {
-    const newName = name.trim();
-    const newEmail = email.trim();
-    const newPassword = password.trim();
-    const keys = Object.keys(req.body);
-    console.log(keys);
-    keys.forEach((key) => {
-      return (req.body[key] = req.body[key].trim());
-    });
-    console.log(req.body);
-    req.body = {
-      ...req.body,
-      name: newName,
-      email: newEmail,
-      password: newPassword,
-    };
-  } else {
-    const newEmail = email.trim();
-    const newPassword = password.trim();
-    req.body = {
-      ...req.body,
-      email: newEmail,
-      password: newPassword,
-    };
-  }
+  // const { name, email, password } = req.body;
+  // if (name) {
+  //   const newName = name.trim();
+  //   const newEmail = email.trim();
+  //   const newPassword = password.trim();
+  const keys = Object.keys(req.body);
+  console.log(keys);
+  keys.forEach((key) => {
+    return (req.body[key] = req.body[key].trim());
+  });
+  console.log(req.body);
+  //   req.body = {
+  //     ...req.body,
+  //     name: newName,
+  //     email: newEmail,
+  //     password: newPassword,
+  //   };
+  // } else {
+  //   const newEmail = email.trim();
+  //   const newPassword = password.trim();
+  //   req.body = {
+  //     ...req.body,
+  //     email: newEmail,
+  //     password: newPassword,
+  //   };
+  // }
   next();
 }
 
