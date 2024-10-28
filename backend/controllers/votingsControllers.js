2463605bfd3099506ceef6d745eb41925f33a04a
VictorFil67 <victorfilinovych@gmail.com>
2024-10-28 14:32:10 +0200

    added get votings request

--- backend/controllers/votingsControllers.js
+++ backend/controllers/votingsControllers.js
@@ -1,6 +1,6 @@
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
 import HttpError from "../helpers/HttpError.js";
-import { addVoting } from "../services/votingsServices.js";
+import { addVoting, votingsList } from "../services/votingsServices.js";
 
 const createVoting = async (req, res) => {
   const user = req.user;
@@ -12,6 +12,19 @@ const createVoting = async (req, res) => {
   res.status(201).json(result);
 };
 
+const getVotings = async (req, res) => {
+  const { role } = req.user;
+  if (role === "not_verified") {
+    throw HttpError(403, "You don't have access to this action!");
+  }
+  const { page = 1, limit = 20, votingType = "Number" } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await votingsList({ skip, limit });
+  console.log(result);
+  res.json(result);
+};
+
 export default {
   createVoting: ctrlWrapper(createVoting),
+  getVotings: ctrlWrapper(getVotings),
 };
--- backend/services/votingsServices.js
+++ backend/services/votingsServices.js
@@ -3,3 +3,7 @@ import Voting from "../models/Voting.js";
 export const addVoting = (data) => {
   return Voting.create(data);
 };
+
+export const votingsList = (query) => {
+  return Voting.find({}, "", query);
+};
