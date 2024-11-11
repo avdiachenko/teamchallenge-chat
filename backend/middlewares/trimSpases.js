675f7c9f410e9f49d60e1f510c19ad19fe0480c5
VictorFil67 <victorfilinovych@gmail.com>
2024-11-11 22:08:43 +0200

    updated

--- backend/middlewares/trimSpases.js
+++ backend/middlewares/trimSpases.js
@@ -1,31 +1,11 @@
 function trimSpases(req, res, next) {
-  // const { name, email, password } = req.body;
-  // if (name) {
-  //   const newName = name.trim();
-  //   const newEmail = email.trim();
-  //   const newPassword = password.trim();
   const keys = Object.keys(req.body);
-  console.log(keys);
-  console.log(Date.now());
+
   keys.forEach((key) => {
     return (req.body[key] = req.body[key].trim());
   });
   console.log(req.body);
-  //   req.body = {
-  //     ...req.body,
-  //     name: newName,
-  //     email: newEmail,
-  //     password: newPassword,
-  //   };
-  // } else {
-  //   const newEmail = email.trim();
-  //   const newPassword = password.trim();
-  //   req.body = {
-  //     ...req.body,
-  //     email: newEmail,
-  //     password: newPassword,
-  //   };
-  // }
+
   next();
 }
 
