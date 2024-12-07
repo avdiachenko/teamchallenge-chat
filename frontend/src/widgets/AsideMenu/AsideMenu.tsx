b1bf07c1047274724ba8e7ac037d3d6cf85b195b
Alexey Lukin <llamerr@gmail.com>
2024-12-07 22:30:07 +0200

    chore: refactor AsideMenu: fix icons, fix styles

--- frontend/src/widgets/AsideMenu/AsideMenu.tsx
+++ frontend/src/widgets/AsideMenu/AsideMenu.tsx
@@ -1,13 +1,15 @@
 import { useUserStore } from "../../entities/user/user.store.ts";
-import styles from "./AsideMenu.module.css";
+import styles from "./AsideMenu.module.scss";
 import { AsideMenuNav } from "./AsideMenuNav/AsideMenuNav.tsx";
 
 export function AsideMenu() {
   const { user } = useUserStore();
 
+  const isLoggedIn = !!user?.name;
+
   return (
     <aside className={styles.aside_menu}>
-      {user?.name ? <AsideMenuNav login={true} /> : <AsideMenuNav login={false} />}
+      <AsideMenuNav isLoggedIn={isLoggedIn} />
     </aside>
   );
 }
--- frontend/src/widgets/AsideMenu/icons/index.ts
+++ frontend/src/widgets/AsideMenu/icons/index.ts
@@ -1,10 +1,19 @@
 import Logo from "./Logo.svg";
-import ChatDots from "./ChatDots.svg";
-import News from "./News.svg";
-import Contact from "./Contacts.svg";
-import Cash from "./Cash.svg";
-import People from "./People.svg";
-import Settings from "./Settings.svg";
-import GoOut from "./GoOut.svg";
+import IconNews from "./NewsIcon.tsx";
+import IconChat from "./ChatIcon.tsx";
+import IconContacts from "./ContactsIcon.tsx";
+import IconPayments from "./PaymentsIcon.tsx";
+import IconNeighbours from "./NeighboursIcon.tsx";
+import IconSettings from "./SettingsIcon.tsx";
+import IconLogout from "./LogoutIcon.tsx";
 
-export { Logo, ChatDots, News, Contact, Cash, People, Settings, GoOut };
+export {
+    Logo,
+    IconNews,
+    IconChat,
+    IconContacts,
+    IconPayments,
+    IconNeighbours,
+    IconSettings,
+    IconLogout,
+};
